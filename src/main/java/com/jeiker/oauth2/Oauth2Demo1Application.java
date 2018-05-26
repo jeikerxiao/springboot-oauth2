3c953c5eef384b083b262144fdfb275266a377c6
jeikerxiao <jeiker@126.com>
2018-05-26 11:35:50 +0800

    init project
    init project

---
+++ src/main/java/com/jeiker/oauth2/Oauth2Demo1Application.java
@@ -0,0 +1,12 @@
+package com.jeiker.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Demo1Application.class, args);
+	}
+}
